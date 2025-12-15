79d38f9f280b852657f32a913fe10df524790283
Sopaco <dokhell@hotmail.com>
2025-12-15 13:16:24 +0800

    remove unused files

--- cortex-mem-insights/test-api.js
+++
@@ -1,8 +0,0 @@
-import { Elysia } from 'elysia';
-
-const app = new Elysia()
-  .get('/', () => 'Hello from Elysia')
-  .get('/health', () => ({ status: 'healthy' }))
-  .listen(3002, () => {
-    console.log('🚀 Test API running on http://localhost:3002');
-  });
